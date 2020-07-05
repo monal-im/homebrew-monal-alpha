8e98ad0bc7539b133d729a37e0773badac5d5657
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2020-07-05 20:41:03 +0200

    Create monal-alpha.rb

---
+++ templates/Casks/monal-alpha.rb
@@ -0,0 +1,12 @@
+cask 'monal-alpha' do
+	version <?=$version?>
+	sha256 <?=$hash?>
+
+	url 'https://www.eightysoft.de/monal/monal.tar'
+	name 'Monal.Alpha'
+	homepage 'https://github.com/tmolitor-stud-tu/monal.alpha'
+
+	depends_on macos: '>= :catalina'
+
+	app 'Monal.alpha.app'
+end
