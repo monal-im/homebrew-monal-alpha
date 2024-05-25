da23a3e3dd3714d065bfca78e0a6edbfe76e5f9e
Thilo Molitor <thilo@eightysoft.de>
2024-05-26 00:10:41 +0200

    Test workaround

--- Casks/monal-alpha.rb
+++ Casks/monal-alpha.rb
@@ -4,7 +4,7 @@ cask "monal-alpha" do
 	sha256 "954cc3316c89469d10fe2c48fc9acf3d793d5a5a6b4ec8a5015d0d479e6e9d6f"
 
 
-	url "https://downloads.monal-im.org/monal-im/alpha/macOS/Monal.alpha.tar"
+	url "https://downloads.monal-im.org/monal-im/alpha/macOS/Monal.alpha.tar?dummy=1716597137"
 	name "Monal.Alpha"
 	homepage "https://github.com/tmolitor-stud-tu/monal.alpha"
 
